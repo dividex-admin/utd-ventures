c21a7ddb66fbe72ab7f2b0044075c074563efafe
unknown <sandeepsng007@gmail.com>
2024-01-09 00:57:09 +0530

    adding validation for 2 steps

--- src/Pages/InvestmentForm/InvestmentForm.js
+++ src/Pages/InvestmentForm/InvestmentForm.js
@@ -41,9 +41,8 @@ const InvestmentForm = () => {
 
   const nextStep = () => {
     let flag=true;
-    if(currentStep==1){
-      flag= validateInput();
-    }
+    flag= validateInput(currentStep);
+    
     if(flag){
       setCurrentStep((prevStep) => prevStep + 1);
     }
@@ -54,37 +53,68 @@ const InvestmentForm = () => {
   };
 
 
-  const validateInput = () => {
+  const validateInput = (step) => {
     const formFields = {...formData};
     const formErrors = {};
     let formIsValid = true;
 
-    if(!formFields["firstName"]){
-      // alert("name cannot be empty");
-      formIsValid = false;
-      formErrors["firstName"] = "Cannot be empty";
-      document.getElementById("firstName").classList.add("border-red-700");
-      document.getElementById("firstNameError").classList.remove("hidden");
-      
+    if(step==1){
+      if(!formFields["firstName"]){
+        // alert("name cannot be empty");
+        formIsValid = false;
+        formErrors["firstName"] = "Cannot be empty";
+        document.getElementById("firstName").classList.add("border-red-700");
+        document.getElementById("firstNameError").classList.remove("hidden");
+        
+      }
+      else{
+        formIsValid = true;
+        document.getElementById("firstName").classList.remove("border-red-700");
+        document.getElementById("firstNameError").classList.add("hidden");
+      }
+      const phoneRegExp = /^[6-9](\d)(?!\1+$)\d{8}$/;
+      if(!formFields["phoneNumber"].match(phoneRegExp)){
+        formIsValid = false;
+        formErrors["phoneNumber"] = "Cannot be empty";
+        document.getElementById("phoneNumber").classList.add("border-red-700");
+        document.getElementById("phoneNumberError").classList.remove("hidden");
+  
+      }
+      else{
+        formIsValid = formIsValid && true;
+        document.getElementById("phoneNumber").classList.remove("border-red-700");
+        document.getElementById("phoneNumberError").classList.add("hidden");
+      }
     }
-    else{
-      formIsValid = true;
-      document.getElementById("firstName").classList.remove("border-red-700");
-      document.getElementById("firstNameError").classList.add("hidden");
-    }
-    const phoneRegExp = /^[6-9](\d)(?!\1+$)\d{8}$/;
-    if(!formFields["phoneNumber"].match(phoneRegExp)){
-      formIsValid = false;
-      formErrors["phoneNumber"] = "Cannot be empty";
-      document.getElementById("phoneNumber").classList.add("border-red-700");
-      document.getElementById("phoneNumberError").classList.remove("hidden");
+    else if(step==2){
+      if(!formFields["projectType"]){
+        formIsValid = false;
+        formErrors["projectType"] = "Cannot be empty";
+        document.getElementById("projectType").classList.add("border-red-700");
+        document.getElementById("projectTypeError").classList.remove("hidden");
+  
+      }
+      else{
+        formIsValid = formIsValid && true;
+        document.getElementById("projectType").classList.remove("border-red-700");
+        document.getElementById("projectTypeError").classList.add("hidden");
+      }
+      if(!formFields["investmentRequired"]){
+        formIsValid = false;
+        formErrors["investmentRequired"] = "Cannot be empty";
+        document.getElementById("investmentRequired").classList.add("border-red-700");
+        document.getElementById("investmentRequiredError").classList.remove("hidden");
+  
+      }
+      else{
+        formIsValid = formIsValid && true;
+        document.getElementById("investmentRequired").classList.remove("border-red-700");
+        document.getElementById("investmentRequiredError").classList.add("hidden");
+      }
+
 
     }
-    else{
-      formIsValid = formIsValid && true;
-      document.getElementById("phoneNumber").classList.remove("border-red-700");
-      document.getElementById("phoneNumberError").classList.add("hidden");
-    }
+    
     return formIsValid;
    };
 
@@ -130,7 +160,7 @@ const InvestmentForm = () => {
   return (
     <>
     <h1 className='mt-4 mb-4 text-center text-3xl font-semibold text-themecolor font-lexend'>List Your Investment Idea </h1>
-    <p className='text-center'>A Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+<p className='text-center mx-6 '>Share your project idea. Dividex team will review it and share it with our investor and operator network to bring it to life!</p>    
     <div className="max-w-5xl border border-solid rounded-[3] mx-auto my-10 p-6 bg-white rounded-md shadow">
          <h2 className='mt-4 mb-8 text-center text-2xl font-semibold font-lexend'>Project Details</h2>
       <div className="mb-20">
@@ -274,7 +304,7 @@ const InvestmentForm = () => {
                 <input 
                   type="text"
                   name="currentIndustry"
-                  placeholder="Industry Currently Working in :"
+                  placeholder="Current Industry :"
                   value={formData.currentIndustry}
                   onChange={handleChange}
                   className="form-input mt-1 p-2 w-full outline-none"
@@ -325,28 +355,34 @@ const InvestmentForm = () => {
             </div>
 
             <div className="flex space-x-4">
-            <div className="w-1/2 my-4 border border-solid rounded-[5]">
-              <label className="px-2 block">Type of Project :</label>
-              <input
-                type="text"
-                name="projectType"
-                placeholder="Type of Project"
-                value={formData.projectType}
-                onChange={handleChange}
-                className="form-input mt-1 w-full p-2 outline-none"
-              />
+            <div className='my-4 w-1/2'>
+              <div id="projectType" className="border border-solid rounded-[5]">
+                <label className="px-2 block">Type of Project :</label>
+                <input
+                  type="text"
+                  name="projectType"
+                  placeholder="Type of Project"
+                  value={formData.projectType}
+                  onChange={handleChange}
+                  className="form-input mt-1 w-full p-2 outline-none"
+                />
+              </div>
+              <div id="projectTypeError" className='hidden font-semibold text-red-700'>Cannot be empty!</div>
             </div>
 
-            <div className="w-1/2 my-4 border border-solid rounded-[5]">
-              <label className="px-2 block">Investment Required (In Rupees) :</label>
-              <input
-                type="number"
-                name="investmentRequired"
-                placeholder="100000"
-                value={formData.investmentRequired}
-                onChange={handleChange}
-                className="form-input mt-1 w-full p-2 outline-none"
-              />
+            <div className='my-4 w-1/2'>
+              <div id="investmentRequired" className="border border-solid rounded-[5]">
+                <label className="px-2 block">Investment Required:</label>
+                <input
+                  type="number"
+                  name="investmentRequired"
+                  placeholder="In rupees"
+                  value={formData.investmentRequired}
+                  onChange={handleChange}
+                  className="form-input mt-1 w-full p-2 outline-none"
+                />
+              </div>
+              <div id="investmentRequiredError" className='hidden font-semibold text-red-700'>Cannot be empty!</div>
             </div>
             </div>
 
@@ -357,7 +393,7 @@ const InvestmentForm = () => {
               <input
                 type="text"
                 name="annualIncome"
-                placeholder="Please mention any recurring income/rental payouts expected"
+                placeholder="Any recurring income/rental payouts expected"
                 value={formData.annualIncome}
                 onChange={handleChange}
                 className="form-input mt-1 w-full p-2 outline-none"
@@ -369,7 +405,7 @@ const InvestmentForm = () => {
               <input
                 type="text"
                 name="expectedPayoutMaturity"
-                placeholder="Please mention any expected Appreciation, payout that would be generated after the projected"
+                placeholder="Any expected Appreciation/rental payout"
                 value={formData.expectedPayoutMaturity}
                 onChange={handleChange}
                 className="form-input mt-1 w-full p-2 outline-none"
@@ -382,7 +418,7 @@ const InvestmentForm = () => {
               <input
                 type="number"
                 name="otherExpectedIncome"
-                placeholder="1000"
+                placeholder="In rupees"
                 value={formData.otherExpectedIncome}
                 onChange={handleChange}
                 className="form-input mt-1 w-full p-2 outline-none"
@@ -394,7 +430,7 @@ const InvestmentForm = () => {
               <input
                 type="number"
                 name="toatalAnnualizedReturn"
-                placeholder="100000"
+                placeholder="In rupees"
                 value={formData.toatalAnnualizedReturn}
                 onChange={handleChange}
                 className="form-input mt-1 w-full p-2 outline-none"
@@ -439,15 +475,21 @@ const InvestmentForm = () => {
             </div>
 
             <div className="my-6 border border-solid rounded-[5]">
+              <div id="projectDuration">
+
               <label className="px-2 block">Timeline for project completion/project duration :</label>
               <input
                 type="text"
                 name="projectDuration"
-                placeholder="4 months"
+                placeholder="In months"
                 value={formData.projectDuration}
                 onChange={handleChange}
                 className="form-input mt-1 w-full p-2 outline-none"
               />
+
+              </div>
+              <div id="projectDurationError" className='hidden font-semibold text-red-700'>Cannot be empty!</div>
+        
             </div>
 
             <div className="my-6 border border-solid rounded-[5]">
@@ -479,7 +521,7 @@ const InvestmentForm = () => {
               <input
                 type="number"
                 name="yourInvestment"
-                placeholder="10000"
+                placeholder="In rupees"
                 value={formData.yourInvestment}
                 onChange={handleChange}
                 className="form-input mt-1 w-full p-2 outline-none"
